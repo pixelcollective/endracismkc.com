aa798dd6f05dad133cbe957d698ada00f5655d08
Kelly Mears <developers@tinypixel.dev>
2021-01-29 15:10:14 -0800

    image optim

--- src/components/partials/Header/index.tsx
+++ src/components/partials/Header/index.tsx
@@ -1,28 +1,29 @@
-const Tag = () => (
+const Chevron = () => (
+  <svg
+    className="w-5 h-5 ml-2 text-gray-500"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    aria-hidden="true">
+    <path
+      fillRule="evenodd"
+      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+const Tag = ({name, description}) => (
   <a
     href="#"
     className="inline-flex items-center p-1 pr-2 text-white bg-gray-100 rounded-full sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
     <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-red-500 to-red-600 rounded-full">
-      #EndRacism
+      {name}
     </span>
 
-    <span className="ml-4 text-sm text-gray-800">
-      Change the name. Stop the chop.
-    </span>
+    <span className="ml-4 text-sm text-gray-800">{description}</span>
 
-    {/* Heroicon name: chevron-right */}
-    <svg
-      className="w-5 h-5 ml-2 text-gray-500"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-      aria-hidden="true">
-      <path
-        fillRule="evenodd"
-        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-        clipRule="evenodd"
-      />
-    </svg>
+    <Chevron />
   </a>
 )
 
@@ -30,6 +31,8 @@ const Background = () => (
   <div className="absolute w-full h-screen min-w-full min-h-screen bg-red-800 -z-10">
     <img
       src="/images/run-it-back.jpg"
+      width="100vw"
+      height="100vh"
       className="top-0 block object-cover w-full h-screen min-w-full min-h-screen opacity-50 filter-blur"
     />
   </div>
@@ -43,11 +46,16 @@ export const Header = () => (
       <div className="container relative z-10 pt-12 pb-24 mx-auto lg:grid">
         <div className="flex items-center px-8 mx-auto md:px-4 sm:text-center lg:text-left">
           <div className="py-24">
-            <Tag />
+            <Tag
+              name="#EndRacism"
+              description="Change the name. Stop the chop."
+            />
+
             <h1 className="max-w-4xl my-4 text-4xl font-medium text-transparent sm:text-6xl lg:mt-6 xl:text-6xl bg-clip-text bg-gradient-to-r from-red-200 to-gray-100">
               <span className="inline-block mb-8 text-4xl tracking-wider">
                 Not In Our Honor Coalition
               </span>
+
               <span className="block font-extrabold text-transparent bg-white bg-clip-text">
                 Change the name of Kansas City's Football Team
               </span>
