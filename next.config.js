bd0c1c729fb1c075f395eee3295f7c39e12857a6
Kelly Mears <developers@tinypixel.dev>
2021-01-29 14:33:14 -0800

    remove md features

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const withMDX = require('@next/mdx')
-
-module.exports = withMDX({
+module.exports = {
   env: [],
   poweredByHeader: false,
   compress: true,
@@ -10,23 +8,7 @@ module.exports = withMDX({
     autoPrerender: false,
   },
   extension: /\.(md|mdx)$/,
-  options: {
-    remarkPlugins: [
-      require('remark-images'),
-      require('remark-emoji'),
-      require('remark-oembed'),
-    ],
-    rehypePlugins: [],
-  },
   experimental: {
     jsconfigPaths: true,
   },
-  exportPathMap: async function (
-    defaultPathMap,
-    {dev, dir, outDir, distDir, buildId},
-  ) {
-    return {
-      '/': {page: './'},
-    }
-  },
-})
+}
