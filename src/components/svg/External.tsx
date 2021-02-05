6749b49de9ade2150a492b1637929d813c82ce02
Kelly Mears <developers@tinypixel.dev>
2021-02-05 09:02:15 -0800

    typings

--- src/components/svg/External.tsx
+++ src/components/svg/External.tsx
@@ -1,4 +1,11 @@
-export const External = ({color, w, h, className, ...props}) => (
+import {FunctionComponent} from 'react'
+
+export const External: FunctionComponent<{
+  color: string,
+  w: number,
+  h: number,
+  className: string,
+}> = ({color, w, h, className, ...props}) => (
   <svg
     className={`${w ? `w-${w}` : `w-5`} ${h ? `h-${h}` : `h-5`} ml-3 -mr-1 ${
       className ?? ``
